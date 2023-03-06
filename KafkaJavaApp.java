c39e8fe8b96385a7f3a817fb7ebf68a18b427b09
MuammerYigitYildiz <87062470+MuammerYigitYildiz@users.noreply.github.com>
2023-03-06 16:36:54 +0300

    dd
    ss

---
+++ KafkaJavaApp.java
@@ -0,0 +1,11 @@
+package com.shubh.kafkachat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaJavaApp {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaJavaApp.class, args);
+    }
+}
