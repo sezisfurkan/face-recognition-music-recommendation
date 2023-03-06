fece232299886a5965ca98b3c0e4e7d151a80be7
Furkan Sezis <sezisfurkan24@gmail.com>
2023-03-06 11:12:01 +0300

    init

---
+++ src/main/java/project/frmr/FrmrApplication.java
@@ -0,0 +1,13 @@
+package project.frmr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrmrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrmrApplication.class, args);
+	}
+
+}
