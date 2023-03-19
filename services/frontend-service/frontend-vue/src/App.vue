50e85e50a6e11c39fbcf5cad29f91c5adae97e6c
Furkan Sezis <sezisfurkan24@gmail.com>
2023-03-19 12:45:13 +0300

    init

--- services/frontend-service/frontend-vue/src/App.vue
+++ services/frontend-service/frontend-vue/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Reaction Timer</h1>
+  <h1>sa</h1>
 </template>
 
 <script>
--- services/frontend-service/frontend-vue/src/main.js
+++ services/frontend-service/frontend-vue/src/main.js
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
 
-createApp(App).mount('#app')
+import App from "./App.vue";
+import PrimeVue from "primevue/config";
+
+import "primevue/resources/themes/saga-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+
+const app = createApp(App);
+
+app.use(PrimeVue);
+
+app.mount("#app");
