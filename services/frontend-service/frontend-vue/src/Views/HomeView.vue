7bf48a99c280f09ec988eed2fad22afb6c5bf2a1
gciftci <gciftci@netas.com.tr>
2023-04-09 16:15:22 +0300

    auth

--- services/frontend-service/frontend-vue/src/Views/HomeView.vue
+++ services/frontend-service/frontend-vue/src/Views/HomeView.vue
@@ -1,10 +1,30 @@
 <template>
-  <h1>sa</h1>
+  <div>
+    <h1>Kamera Aç/Kapat</h1>
+    <button @click="openCamera">Kamera Aç</button>
+    <button @click="closeCamera">Kamera Kapat</button>
+    <video ref="videoElement" autoplay></video>
+  </div>
 </template>
 <script>
 export default {
-  mounted() {
-
+  methods: {
+    async openCamera() {
+      try {
+        this.stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        this.$refs.videoElement.srcObject = this.stream;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    closeCamera() {
+      if (this.stream) {
+        const tracks = this.stream.getTracks();
+        tracks.forEach(track => track.stop());
+        this.$refs.videoElement.srcObject = null;
+        this.stream = null;
+      }
+    }
   }
 }
 </script>
