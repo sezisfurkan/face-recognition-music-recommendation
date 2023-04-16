bd851c505e1a5ef1dd6ce88f64f44a179f8d3847
ardaerpak <arda-erpak@hotmail.com>
2023-04-16 15:11:46 +0300

    relations in song and emotion

--- services/backend-service/src/main/java/project/frmr/entity/Emotion.java
+++ services/backend-service/src/main/java/project/frmr/entity/Emotion.java
@@ -8,6 +8,8 @@ import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 import project.frmr.models.ModifiableEntity;
 
+import java.util.List;
+
 
 @Getter
 @Setter
@@ -20,8 +22,13 @@ public class Emotion extends ModifiableEntity {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name="UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name="emotion_id")
     private String id;
 
     @Column(nullable = false, length = 100)
     private String emotionName;
+
+    @OneToMany(mappedBy = "emotion")
+    private List<Song> songs;
+
 }
