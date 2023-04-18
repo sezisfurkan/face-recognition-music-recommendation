6299d8d1e8f38f01748d9a8d7b843edd3cdd191f
yigityildiz <muammeryigityildiz@gmail.com>
2023-04-18 14:31:16 +0300

    video player with python

--- services/frontend-service/frontend-vue/src/Views/HomeView.vue
+++ services/frontend-service/frontend-vue/src/Views/HomeView.vue
@@ -1,36 +1,4 @@
-<!--  <template>
-    <div>
-      <h1 style="color: #fff;">Kamera Aç/Kapat</h1>
-      <button @click="openCamera">Kamera Aç</button>
-      <button @click="closeCamera">Kamera Kapat</button>
-      <video ref="videoElement" autoplay></video>
-    </div>
-  </template>
-  <script>
-  export default {
-    methods: {
-      async openCamera() {
-        try {
-          this.stream = await navigator.mediaDevices.getUserMedia({ video: true });
-          this.$refs.videoElement.srcObject = this.stream;
-        } catch (error) {
-          console.error(error);
-        }
-      },
-      closeCamera() {
-        if (this.stream) {
-          const tracks = this.stream.getTracks();
-          tracks.forEach(track => track.stop());
-          this.$refs.videoElement.srcObject = null;
-          this.stream = null;
-        }
-      }
-    }
-  }
-  </script>
-  <style>
 
-  </style>-->
 
 <template>
   <div class="menubar">
@@ -52,6 +20,7 @@ export default {
   methods: {
     async openCamera() {
       try {
+       /*const response = await fetch('http://127.0.0.1:5000/start', { method: 'POST' });*/
         this.stream = await navigator.mediaDevices.getUserMedia({ video: true });
         this.$refs.videoElement.srcObject = this.stream;
         this.intervalId = setInterval(this.detectFaces, 1000);
@@ -62,13 +31,14 @@ export default {
     closeCamera() {
       clearInterval(this.intervalId);
       if (this.stream) {
+
         const tracks = this.stream.getTracks();
         tracks.forEach(track => track.stop());
         this.$refs.videoElement.srcObject = null;
         this.stream = null;
       }
     },
-    async detectFaces() {
+ /*   async detectFaces() {
       try {
         const canvas = document.createElement('canvas');
         const video = this.$refs.videoElement;
@@ -93,16 +63,70 @@ export default {
       } catch (error) {
         console.error(error);
       }
+    }*/
+   /* async detectFaces() {
+      try {
+        const canvas = document.createElement('canvas');
+        const video = this.$refs.videoElement;
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+        const blob = await new Promise(resolve => canvas.toBlob(resolve, 'image/jpeg', 0.95));
+        const formData = new FormData();
+        formData.append('image', blob, 'image.jpg');
+        const response = await fetch('http://127.0.0.1:5000/detect', {
+          method: 'POST',
+          mode: 'no-cors',
+          body: formData
+        });
+        const data = await response.json(); // .blob() yerine .json() kullanın
+        const content = data.content;
+        const base64ImageData = 'data:image/jpeg;base64,' + content;
+        const updatedImage = new Image();
+        updatedImage.src = base64ImageData;
+        updatedImage.onload = () => {
+          ctx.clearRect(0, 0, canvas.width, canvas.height);
+          ctx.drawImage(updatedImage, 0, 0, canvas.width, canvas.height);
+        };
+      } catch (error) {
+        console.error(error);
+      }
+    }*/
+    async detectFaces() {
+      try {
+        const canvas = document.createElement('canvas');
+        const video = this.$refs.videoElement;
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+        const blob = await new Promise(resolve => canvas.toBlob(resolve, 'image/jpeg', 0.95));
+        const formData = new FormData();
+        formData.append('image', blob, 'image.jpg');
+        const response = await fetch('http://127.0.0.1:5000/detect', {
+          method: 'POST',
+          mode: 'no-cors',
+          body: formData
+        });
+
+        if (response.status === 500) {
+          const errorData = await response.json();
+          console.error('Error from server:', errorData);
+          return;
+        }
+
+        const data = await response.blob();
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          this.$refs.videoElement.src = reader.result;
+        };
+        reader.readAsDataURL(data);
+      } catch (error) {
+        console.error(error);
+      }
     }
   }
 };
 
 </script>
-
-<style scoped>
-.menubar {
-    display: block;
-    justify-content: center;
-    align-items: center;
-}
-</style>
