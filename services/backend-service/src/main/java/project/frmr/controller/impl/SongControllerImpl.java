2f089ea3b0b4b97f3fad61b4b8db5ff739fc610e
ardaerpak <arda-erpak@hotmail.com>
2023-04-19 14:38:36 +0300

    SongControllerImpl created

--- services/backend-service/src/main/java/project/frmr/controller/impl/SongControllerImpl.java
+++ services/backend-service/src/main/java/project/frmr/controller/impl/SongControllerImpl.java
@@ -3,17 +3,19 @@ package project.frmr.controller.impl;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import project.frmr.controller.SongController;
 import project.frmr.dto.SongDTO;
+import project.frmr.entity.Song;
 import project.frmr.mapper.SongMapper;
 import project.frmr.service.SongService;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/api/v1")
@@ -26,8 +28,11 @@ public class SongControllerImpl implements SongController {
     SongMapper songMapper;
 
     @Override
-    public SongDTO save(SongDTO user) {
-        return null;
+    @PostMapping("/song")
+    @ResponseStatus(HttpStatus.CREATED)
+    public SongDTO save(SongDTO songDTO) {
+        Song song = songMapper.asEntity(songDTO);
+        return songMapper.asDTO(songService.save(song));
     }
 
     @Override
@@ -36,27 +41,38 @@ public class SongControllerImpl implements SongController {
     }
 
     @Override
-    public SongDTO findById(String id) {
-        return null;
+    @GetMapping("/song/{id}")
+    public SongDTO findById(@PathVariable("id") String id) {
+        Song song = songService.findById(id).orElse(null);
+        return songMapper.asDTO(song);
     }
 
     @Override
-    public void delete(String id) {
-
+    @DeleteMapping("/song/{id}")
+    public void delete(@PathVariable("id") String id) {
+        songService.deleteById(id);
     }
 
     @Override
+    @GetMapping("/song")
     public List<SongDTO> list() {
-        return null;
+        return songMapper.asDTOList(songService.findAll());
     }
 
     @Override
+    @GetMapping("/page-query")
     public Page<SongDTO> pageQuery(Pageable pageable) {
-        return null;
+        Page<Song> songPage = songService.findAll(pageable);
+        List<SongDTO> dtoList = songPage
+                .stream()
+                .map(songMapper::asDTO).collect(Collectors.toList());
+        return new PageImpl<>(dtoList, pageable, songPage.getTotalElements());
     }
 
     @Override
-    public SongDTO update(SongDTO dto, String id) {
-        return null;
+    @PutMapping("/song/{id}")
+    public SongDTO update(SongDTO songDTO, @PathVariable("id") String id) {
+       Song song = songMapper.asEntity(songDTO);
+        return songMapper.asDTO(songService.update(song, id));
     }
 }
